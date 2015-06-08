f41a7338579e39ecd984c0f4ad16b64e5c0cc306
Alberto A. Ocampo <alocampor@gmail.com>
2015-06-08 18:14:56 +0200

    Adding the digi filter

--- MyProducer/MyRPCProducer/python/CfiFile_cfi.py
+++ MyProducer/MyRPCProducer/python/CfiFile_cfi.py
@@ -1,5 +1,4 @@
 import FWCore.ParameterSet.Config as cms
 
-demo = cms.EDProducer('MyRPCProducer',
-                      src = cms.InputTag('AhmedRecHitCollection')
+demo = cms.EDProducer('MyRPCProducer'
 )
